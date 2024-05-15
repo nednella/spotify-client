237056e29a774af600a923dcb5b8aee11e3ca21a
nednella <119449811+nednella@users.noreply.github.com>
2024-05-15 19:39:56 +0100

    Add: page routing with placeholder pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
+import { Route, Routes } from 'react-router-dom'
+
 import Sidebar from './components/Sidebar'
+
 import Home from './pages/Home'
+import Search from './pages/Search'
+import Account from './pages/Account'
+import NotFound from './pages/NotFound'
 
 const App = () => {
+    const routes = [
+        {
+            path: '/',
+            element: <Home />,
+        },
+        {
+            path: '/search',
+            element: <Search />,
+        },
+        {
+            path: '/account',
+            element: <Account />,
+        },
+        {
+            path: '*',
+            element: <NotFound />,
+        },
+    ]
+
     return (
         <div className="flex h-full">
             <Sidebar />
-            <Home />
+            <Routes>
+                {routes.map((route) => (
+                    <Route {...route} />
+                ))}
+            </Routes>
         </div>
     )
 }
