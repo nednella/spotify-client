a05264eea16d2c7d81523a4b9073cd65996a199d
Ben Allenden <benallenden@protonmail.com>
2024-05-15 19:39:56 +0100

    Add: page routing with placeholder pages

---
+++ src/pages/Account.tsx
@@ -0,0 +1,9 @@
+const Account = () => {
+    return (
+        <div className="flex h-full flex-1 flex-col items-center justify-center">
+            <h1 className="text-3xl font-semibold text-white">Account Page</h1>
+        </div>
+    )
+}
+
+export default Account
