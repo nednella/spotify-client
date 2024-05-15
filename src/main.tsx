ac24b3c79b2854707535f70af8877de554faeb6b
nednella <119449811+nednella@users.noreply.github.com>
2024-05-15 03:50:35 +0100

    Initial project commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import './main.css'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-		<h1>Spotify Web App</h1>
-	</React.StrictMode>
+    <React.StrictMode>
+        <>
+            <div className='flex h-full w-full flex-col items-center justify-center'>
+                <h1 className='text-3xl font-semibold text-white'>Spotify Web Player</h1>
+            </div>
+        </>
+    </React.StrictMode>
 )
