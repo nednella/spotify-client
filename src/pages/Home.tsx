3cadd2d902b948058fc3c2c794dec676b9d7d670
Ben Allenden <benallenden@protonmail.com>
2024-05-16 23:24:09 +0100

    Add: small header styling adjustments

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,18 @@
 import Header from '../components/Header'
-// import Test from '../components/TEST'
 
 const Home = () => {
+    const user = false
+
     return (
+        // to-33 bg-gradient-to-b from-emerald-800
         <>
-            <Header className="">
-                <h1 className="text-3xl font-semibold text-white">Welcome back, user</h1>
-                <h3>[Additional Nav Buttons]</h3>
+            <Header className="to-emerald-800">
+                {user ? (
+                    <>
+                        <h1 className="text-3xl font-semibold text-white">Welcome back, user</h1>
+                        <h3>[Additional Nav Buttons]</h3>
+                    </>
+                ) : null}
             </Header>
             <div className="flex items-center justify-center">
                 <h1 className="text-3xl font-semibold text-white">Home Page</h1>
