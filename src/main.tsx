912ab8ed7ad7a155b232514221cb1de697a29727
Ben Allenden <benallenden@protonmail.com>
2024-05-17 04:43:31 +0100

    Refactor: file structure

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import './main.css'
-import App from './App'
+import App from './app/App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
