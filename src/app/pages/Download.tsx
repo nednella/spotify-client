cf4de3645d55344e7e6defcbd79ce002c53a1da4
Ben Allenden <benallenden@protonmail.com>
2024-05-18 00:00:44 +0100

    Feat: download app page

--- src/app/pages/Download.tsx
+++ src/app/pages/Download.tsx
@@ -1,11 +1,32 @@
+import Button from '../../components/Button'
 import Header from '../../components/Header'
 
 const Download = () => {
     return (
         <>
-            <Header />
-            <div className="flex items-center justify-center">
-                <h1 className="text-3xl font-semibold text-white">Download Page</h1>
+            <Header className="bg-none" />
+            <div
+                className="flex flex-grow flex-col items-center justify-center bg-blue-900"
+                style={{ backgroundImage: 'linear-gradient(to bottom,rgba(0,0,0,.3),#171717 40%)' }}
+            >
+                <div className="w-[640px] max-w-[80%]">
+                    <img
+                        className="object-cover"
+                        src="src/assets/images/mac.png"
+                    />
+                    <div className="flex flex-col items-center">
+                        <h2 className="xsm:text-3xl text-center text-xl font-bold">
+                            Seamlessly listen the music you love. Download the Spotify app for your
+                            computer.
+                        </h2>
+                        <a
+                            href="https://spotify.com/download"
+                            rel="noreferrer"
+                        >
+                            <Button className="mt-8 w-[200px]">Get the free app</Button>
+                        </a>
+                    </div>
+                </div>
             </div>
         </>
     )
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -9,7 +9,7 @@ interface ContentProps {
 const Content: React.FC<ContentProps> = ({ children }) => {
     return (
         <div className="h-full flex-1 overflow-hidden py-2 pr-2">
-            <Container className="h-full overflow-y-auto">{children}</Container>
+            <Container className="flex h-full flex-col overflow-y-auto">{children}</Container>
         </div>
     )
 }
