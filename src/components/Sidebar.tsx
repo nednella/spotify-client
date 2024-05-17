758ad02e1e5f587d491bb0d499ff72a6ac43bbd0
nednella <119449811+nednella@users.noreply.github.com>
2024-05-17 23:42:32 +0100

    Add: sidebar width breakpoint

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -25,7 +25,7 @@ const Sidebar = () => {
     ]
 
     return (
-        <div className="hidden h-full w-[300px] flex-col gap-y-2 p-2 md:flex">
+        <div className="hidden h-full w-[300px] flex-col gap-y-2 p-2 md:flex lg:w-[350px]">
             <Container className="flex flex-col gap-y-4 px-6 py-4">
                 {links.map((link) => (
                     <SidebarItem
