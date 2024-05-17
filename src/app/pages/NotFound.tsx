ed356820792a94d907f8ce1c97c500c28277394f
Ben Allenden <benallenden@protonmail.com>
2024-05-18 00:12:06 +0100

    Feat: not found page

--- src/app/pages/NotFound.tsx
+++ src/app/pages/NotFound.tsx
@@ -3,9 +3,13 @@ import Header from '../../components/Header'
 const NotFound = () => {
     return (
         <>
-            <Header />
-            <div className="flex items-center justify-center">
-                <h1 className="text-3xl font-semibold text-white">Not Found Page</h1>
+            <Header className="bg-none" />
+            <div className="flex flex-grow flex-col items-center justify-center">
+                <img src="/public/spotify-icon.svg" />
+                <h1 className="mt-8 text-5xl font-bold">Page not found</h1>
+                <p className="mt-2 text-neutral-400">
+                    We can't seem to find the page you are looking for.
+                </p>
             </div>
         </>
     )
