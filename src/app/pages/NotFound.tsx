be30ae3f718eeedbdc87fd827d66a1375fa2c6db
nednella <119449811+nednella@users.noreply.github.com>
2024-05-18 18:25:49 +0100

    Fix: icon path

--- src/app/pages/NotFound.tsx
+++ src/app/pages/NotFound.tsx
@@ -5,7 +5,7 @@ const NotFound = () => {
         <>
             <Header className="bg-none" />
             <div className="flex flex-grow flex-col items-center justify-center">
-                <img src="/public/spotify-icon.svg" />
+                <img src="/spotify-icon.svg" />
                 <h1 className="mt-8 text-5xl font-bold">Page not found</h1>
                 <p className="mt-2 text-neutral-400">
                     We can't seem to find the page you are looking for.
