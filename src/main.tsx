ef42b200e17aa0fb5d6aee889abf41bf2881ebac
Ben Allenden <benallenden@protonmail.com>
2024-05-22 20:24:24 +0100

    Feat: Spotify API authorisation

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,16 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import ModalProvider from './providers/ModalProvider'
+import ToastProvider from './providers/ToastProvider'
 
 import './main.css'
 import App from './app/App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <ModalProvider />
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>
+    <BrowserRouter>
+        <ModalProvider />
+        <ToastProvider />
+        <App />
+    </BrowserRouter>
 )
