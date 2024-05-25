1c8474afa4ea36dab9b878b43da47afbf38b6d18
Ben Allenden <benallenden@protonmail.com>
2024-05-25 03:57:25 +0100

    Feat: library items, empty and loading components

---
+++ src/components/library/LibraryEmpty.tsx
@@ -0,0 +1,78 @@
+import React from 'react'
+
+import Button from '../Button'
+import Container from '../Container'
+
+interface LibraryEmptyProps {
+    fns: (() => void)[]
+}
+
+const LibraryEmpty: React.FC<LibraryEmptyProps> = ({ fns: [createPlaylist, searchPodcasts] }) => {
+    return (
+        <div
+            className="
+                flex
+                h-full
+                flex-grow
+                flex-col
+                justify-between
+                gap-y-2
+            "
+        >
+            <div className="flex flex-col gap-y-4">
+                <Container className="select-none overflow-hidden bg-neutral-800 p-4">
+                    <h3 className="textlg truncate font-bold">Create your first playlist</h3>
+                    <p className="mt-2 text-sm font-semibold">It's easy, we'll help you</p>
+                    <Button
+                        onClick={createPlaylist}
+                        className="mt-4 w-fit bg-white px-4 py-1"
+                    >
+                        Create playlist
+                    </Button>
+                </Container>
+                <Container className="select-none overflow-hidden bg-neutral-800 p-4">
+                    <h3 className="textlg truncate text-wrap font-bold">
+                        Let's find some podcasts to follow
+                    </h3>
+                    <p className="mt-2 text-sm font-semibold">
+                        We'll keep you updated on new episodes
+                    </p>
+                    <Button
+                        onClick={searchPodcasts}
+                        className="mt-4 w-fit bg-white px-4 py-1"
+                    >
+                        Browse podcasts
+                    </Button>
+                </Container>
+            </div>
+
+            <div
+                className="
+                    w-ful
+                    h-fit
+                    select-none
+                    p-6
+                    pb-12
+                    text-xs
+                    font-medium
+                    text-neutral-400
+                "
+            >
+                <div className="flex flex-wrap">
+                    <p className="cursor-pointer py-2 pr-3">Legal</p>
+                    <p className="cursor-pointer py-2 pr-3">Safety & Privacy Center</p>
+                    <p className="cursor-pointer py-2 pr-3">Privacy Policy</p>
+                    <p className="cursor-pointer py-2 pr-3">Cookie Settings</p>
+                    <p className="cursor-pointer py-2 pr-3">About Ads</p>
+                    <p className="cursor-pointer py-2 pr-3">Accessibility</p>
+                    <p className="cursor-pointer py-2 pr-3">Modern Slavery Act</p>
+                    <p className="cursor-pointer py-2 pr-3">UK Tax Policy</p>
+                    <p className="cursor-pointer py-2 pr-3">UK Gender Pay Report</p>
+                </div>
+                <p className="cursor-pointer pt-2 text-sm">Cookies</p>
+            </div>
+        </div>
+    )
+}
+
+export default LibraryEmpty
---
+++ src/components/library/LibraryHeader.tsx
@@ -0,0 +1,42 @@
+import { TbPlaylist } from 'react-icons/tb'
+import { AiOutlinePlus } from 'react-icons/ai'
+import React from 'react'
+
+interface LibraryHeaderProps {
+    fns: (() => void)[]
+}
+
+const LibraryHeader: React.FC<LibraryHeaderProps> = ({ fns: [createPlaylist] }) => {
+    return (
+        <>
+            <div
+                className="
+                    sticky
+                    top-0
+                    flex
+                    select-none
+                    items-center
+                    justify-between
+                    bg-neutral-900
+                    px-5
+                    py-4
+                "
+            >
+                <div className="inline-flex items-center gap-x-2">
+                    <TbPlaylist
+                        size={26}
+                        className="text-neutral-400"
+                    />
+                    <p className="text-md font-medium text-neutral-400">Your Library</p>
+                </div>
+                <AiOutlinePlus
+                    onClick={createPlaylist}
+                    size={20}
+                    className="cursor-pointer text-neutral-400 hover:text-white"
+                />
+            </div>
+        </>
+    )
+}
+
+export default LibraryHeader
