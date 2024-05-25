1754539f2b4efb0c9bc64edf9f97f589c68193c1
Ben Allenden <benallenden@protonmail.com>
2024-05-25 23:16:33 +0100

    Refactor: remove container rounded corners in mobile view

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -9,7 +9,7 @@ interface ContentProps {
 const Content: React.FC<ContentProps> = ({ children }) => {
     return (
         <div className="h-full flex-1 overflow-hidden md:py-2 md:pr-2">
-            <Container className="relative flex h-full flex-col overflow-y-auto">
+            <Container className="relative flex h-full flex-col overflow-y-auto rounded-none md:rounded-lg">
                 {children}
             </Container>
         </div>
