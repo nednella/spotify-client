337eab19a219e99afbb90037c45fe928668c8ede
nednella <119449811+nednella@users.noreply.github.com>
2024-05-27 05:05:40 +0100

    Feat: pull user data into account page

--- src/app/pages/Account.tsx
+++ src/app/pages/Account.tsx
@@ -1,10 +1,12 @@
 import { FaUserAlt } from 'react-icons/fa'
 
+import { useAuth } from '../../hooks/useAuth'
+
 import Container from '../../components/Container'
 import Header from '../../components/Header'
 
 const Account = () => {
-    // TODO: fetch user data
+    const { user } = useAuth()
 
     return (
         <>
@@ -21,7 +23,9 @@ const Account = () => {
                         "
                         >
                             <p className="mb-2 text-xs font-normal">Your Plan</p>
-                            <p className="text-3xl font-bold text-pink-200/90">[product] user</p>
+                            <p className="pl-4 text-3xl font-bold text-pink-200/90">
+                                {user?.product} user
+                            </p>
                         </Container>
                         <a
                             href="https://spotify.com/account"
@@ -54,24 +58,24 @@ const Account = () => {
                     "
                     >
                         <p className="mb-4 text-2xl font-bold">User Details</p>
-                        <div className="px-4">
+                        <div className="pl-4">
                             <div>
                                 <p className="text-xs font-medium text-neutral-400">Username</p>
-                                <p className="mb-3 text-lg font-bold">[username]</p>
+                                <p className="mb-3 text-lg font-bold">{user?.id}</p>
                             </div>
                             <div>
                                 <p className="text-xs font-medium text-neutral-400">
                                     Email address
                                 </p>
-                                <p className="mb-3 text-lg font-bold">[email_address]</p>
+                                <p className="mb-3 text-lg font-bold">{user?.email}</p>
                             </div>
                             <div>
                                 <p className="text-xs font-medium text-neutral-400">Display name</p>
-                                <p className="mb-3 text-lg font-bold">[display_name]</p>
+                                <p className="mb-3 text-lg font-bold">{user?.display_name}</p>
                             </div>
                             <div>
                                 <p className="text-xs font-medium text-neutral-400">Country</p>
-                                <p className="text-lg font-bold">[country]</p>
+                                <p className="text-lg font-bold">{user?.country}</p>
                             </div>
                         </div>
                     </Container>
@@ -85,7 +89,7 @@ const Account = () => {
                         <p className="mb-4 text-2xl font-bold">
                             If you'd like to revoke access to this app
                         </p>
-                        <div className="px-4">
+                        <div className="pl-4">
                             <p className="mb-3 font-semibold text-neutral-400">
                                 1. Click on "Your account" to redirect to the real Spotify account
                                 settings page
