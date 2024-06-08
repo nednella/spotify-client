9120a0fc0eae3b2f271fa37135ac61b229ac34d3
Ben Allenden <benallenden@protonmail.com>
2024-06-08 18:39:21 +0100

    Feat: scroll area component

---
+++ src/components/ScrollArea.tsx
@@ -0,0 +1,22 @@
+import * as ScrollArea from '@radix-ui/react-scroll-area'
+import React from 'react'
+import { twMerge } from 'tailwind-merge'
+
+interface ScrollProps {
+    children: React.ReactNode
+    className?: string
+}
+
+const Scroll: React.FC<ScrollProps> = ({ children, className }) => (
+    <ScrollArea.Root className={twMerge('overflow-hidden', className)}>
+        <ScrollArea.Viewport className="h-full w-full">{children}</ScrollArea.Viewport>
+        <ScrollArea.Scrollbar
+            className="touch-none select-none data-[orientation=horizontal]:h-3 data-[orientation=vertical]:w-3 data-[orientation=horizontal]:flex-col"
+            orientation="vertical"
+        >
+            <ScrollArea.Thumb className="w-10 bg-neutral-400/50 hover:bg-neutral-400/70" />
+        </ScrollArea.Scrollbar>
+    </ScrollArea.Root>
+)
+
+export default Scroll
