f5f2117365e8f0f164517cdabae4ed33683980ab
nednella <119449811+nednella@users.noreply.github.com>
2024-06-10 21:47:40 +0100

    Feat: header opacity animation on scroll using framer-motion

--- src/components/ScrollArea.tsx
+++ src/components/ScrollArea.tsx
@@ -9,33 +9,37 @@ interface ScrollProps {
 
 // TODO: allow scrollbar sizing to adjust every time the library size changes (on click of a category)
 
-const Scroll: React.FC<ScrollProps> = ({ children, className, ...props }) => {
-    return (
-        <ScrollArea.Root
-            // TODO: uncomment - type="scroll"
-            className={twMerge('overflow-hidden', className)}
-            {...props}
-        >
-            <ScrollArea.Viewport
-                asChild
-                className="!block h-full w-full"
+const Scroll = React.forwardRef<HTMLDivElement, ScrollProps>(
+    ({ children, className, ...props }, forwardedRef) => {
+        return (
+            <ScrollArea.Root
+                // TODO: uncomment - type="scroll"
+                className={twMerge('overflow-hidden', className)}
+                {...props}
             >
-                {children}
-            </ScrollArea.Viewport>
-            <ScrollArea.Scrollbar
-                className="
-                    touch-none
-                    select-none
-                    data-[orientation=horizontal]:h-3
-                    data-[orientation=vertical]:w-3
-                    data-[orientation=horizontal]:flex-col
-                "
-                orientation="vertical"
-            >
-                <ScrollArea.Thumb className="w-full bg-neutral-400/50 hover:bg-neutral-400/70" />
-            </ScrollArea.Scrollbar>
-        </ScrollArea.Root>
-    )
-}
+                <ScrollArea.Viewport
+                    ref={forwardedRef}
+                    asChild
+                    className="!block h-full w-full"
+                >
+                    {children}
+                </ScrollArea.Viewport>
+                <ScrollArea.Scrollbar
+                    className="
+                        z-[999]
+                        touch-none
+                        select-none
+                        data-[orientation=horizontal]:h-3
+                        data-[orientation=vertical]:w-3
+                        data-[orientation=horizontal]:flex-col
+                    "
+                    orientation="vertical"
+                >
+                    <ScrollArea.Thumb className="w-full bg-neutral-400/50 hover:bg-neutral-400/70" />
+                </ScrollArea.Scrollbar>
+            </ScrollArea.Root>
+        )
+    }
+)
 
 export default Scroll
