d6e1e063a06654f512782e8b9ad8fd7646d7b0fe
nednella <119449811+nednella@users.noreply.github.com>
2024-06-10 19:56:34 +0100

    Refactor: homepage using scroll area

--- src/components/ScrollArea.tsx
+++ src/components/ScrollArea.tsx
@@ -1,5 +1,5 @@
-import * as ScrollArea from '@radix-ui/react-scroll-area'
 import React from 'react'
+import * as ScrollArea from '@radix-ui/react-scroll-area'
 import { twMerge } from 'tailwind-merge'
 
 interface ScrollProps {
@@ -9,30 +9,33 @@ interface ScrollProps {
 
 // TODO: allow scrollbar sizing to adjust every time the library size changes (on click of a category)
 
-const Scroll: React.FC<ScrollProps> = ({ children, className, ...props }) => (
-    <ScrollArea.Root
-        className={twMerge('overflow-hidden', className)}
-        {...props}
-    >
-        <ScrollArea.Viewport
-            asChild
-            className="!block h-full w-full"
+const Scroll: React.FC<ScrollProps> = ({ children, className, ...props }) => {
+    return (
+        <ScrollArea.Root
+            // TODO: uncomment - type="scroll"
+            className={twMerge('overflow-hidden', className)}
+            {...props}
         >
-            {children}
-        </ScrollArea.Viewport>
-        <ScrollArea.Scrollbar
-            className="
-                touch-none
-                select-none
-                data-[orientation=horizontal]:h-3
-                data-[orientation=vertical]:w-3
-                data-[orientation=horizontal]:flex-col
-            "
-            orientation="vertical"
-        >
-            <ScrollArea.Thumb className="w-10 bg-neutral-400/50 hover:bg-neutral-400/70" />
-        </ScrollArea.Scrollbar>
-    </ScrollArea.Root>
-)
+            <ScrollArea.Viewport
+                asChild
+                className="!block h-full w-full"
+            >
+                {children}
+            </ScrollArea.Viewport>
+            <ScrollArea.Scrollbar
+                className="
+                    touch-none
+                    select-none
+                    data-[orientation=horizontal]:h-3
+                    data-[orientation=vertical]:w-3
+                    data-[orientation=horizontal]:flex-col
+                "
+                orientation="vertical"
+            >
+                <ScrollArea.Thumb className="w-full bg-neutral-400/50 hover:bg-neutral-400/70" />
+            </ScrollArea.Scrollbar>
+        </ScrollArea.Root>
+    )
+}
 
 export default Scroll
