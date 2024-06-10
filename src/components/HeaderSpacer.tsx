dd2c70e43913d26bad7b12a00328a703fa0c0bcf
Ben Allenden <benallenden@protonmail.com>
2024-06-10 21:49:48 +0100

    Add: header spacer component

---
+++ src/components/HeaderSpacer.tsx
@@ -0,0 +1,7 @@
+// Prevents page content from laying underneath the header where applicable
+
+const HeaderSpacer = () => {
+    return <div className="h-[64px] w-full"></div>
+}
+
+export default HeaderSpacer
