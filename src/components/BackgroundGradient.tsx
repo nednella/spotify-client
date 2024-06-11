3cd561894692d40fa8eb8b4eadbc3c66096a12aa
Ben Allenden <benallenden@protonmail.com>
2024-06-11 02:53:44 +0100

    Add: className prop to bgGradient component

--- src/components/BackgroundGradient.tsx
+++ src/components/BackgroundGradient.tsx
@@ -2,20 +2,24 @@ import React from 'react'
 import { twMerge } from 'tailwind-merge'
 
 interface BackgroundGradientProps {
-    colour: string
+    colour?: string
+    className?: string
     size?: string
 }
 
-const BackgroundGradient: React.FC<BackgroundGradientProps> = ({ colour, size }) => {
+const BackgroundGradient: React.FC<BackgroundGradientProps> = ({ colour, className, size }) => {
     return (
         <div
             className={twMerge(
                 'absolute top-0 w-full',
+                className,
                 size === 'large' ? 'h-[400px]' : size === 'medium' ? 'h-[300px]' : 'h-[200px]'
             )}
             style={{
                 backgroundColor: `rgb(${colour})`,
-                backgroundImage: 'linear-gradient(rgba(0, 0, 0, .6) 0, rgb(23, 23, 23) 100%)',
+                backgroundImage: colour
+                    ? 'linear-gradient(rgba(0, 0, 0, .6) 0, rgb(23, 23, 23) 100%)'
+                    : '',
             }}
         ></div>
     )
