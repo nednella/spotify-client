16a25ad6a9f179e21ed64d6bc593a546165d1fe9
nednella <119449811+nednella@users.noreply.github.com>
2024-06-12 02:58:48 +0100

    Refactor: separator component now requires orientation prop

--- src/components/Separator.tsx
+++ src/components/Separator.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 import { twMerge } from 'tailwind-merge'
 
 interface SeparatorProps {
+    orientation: 'horizontal' | 'vertical'
     className: string
 }
 
-const Separator: React.FC<SeparatorProps> = ({ className }) => {
+const Separator: React.FC<SeparatorProps> = ({ orientation, className }) => {
     return (
         <RSeparator.Root
+            orientation={orientation}
             className={twMerge(
                 `
                     data-[orientation=horizontal]:h-px
