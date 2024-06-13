733b084fff2597f7e862b8bc4dda3621093f3640
nednella <119449811+nednella@users.noreply.github.com>
2024-06-13 22:27:45 +0100

    Add: playlist & profile content wrapper components

--- src/app/pages/Playlist.tsx
+++ src/app/pages/Playlist.tsx
@@ -1,109 +1,21 @@
-import Header from '../../components/Header'
+import { useEffect, useState } from 'react'
+
+import PlaylistWrapper from '../../components/content/PlaylistWrapper'
+import ContentSectionLoading from '../../components/homepage/ContentSectionLoading'
 
 const Playlist = () => {
+    const [colour, setColour] = useState<string | undefined>(undefined) // accepts 'r/g/b' format
+
+    useEffect(() => {
+        setColour('86, 58, 204')
+    }, [])
+
     return (
         <>
-            <Header />
-            <main
-                className="
-                    flex
-                    flex-grow
-                    flex-col
-                    bg-gradient-to-b
-                    from-red-800
-                    to-50vh
-                "
-            >
-                {/* // Playlist info */}
-                <div
-                    className="
-                        mb-4
-                        mt-8
-                        flex
-                        flex-col
-                        bg-transparent
-                        px-4
-                        md:flex-row
-                    "
-                >
-                    <div
-                        className="
-                            mb-4
-                            w-[50%]
-                            min-w-[128px]
-                            max-w-[240px]
-                            self-center
-                            md:mb-0
-                            md:mr-4
-                            md:max-h-[128px]
-                            md:max-w-[128px]
-                            md:self-end
-                        "
-                    >
-                        <img
-                            className="rounded-md object-cover"
-                            src="src/assets/images/liked.png"
-                            alt=""
-                        />
-                    </div>
-                    <div
-                        className="
-                            flex
-                            flex-col
-                            gap-y-2
-                            md:justify-end
-                            md:gap-0
-                        "
-                    >
-                        <span className="hidden truncate text-sm font-semibold md:block">Playlist</span>
-                        <span className="truncate text-wrap text-4xl font-black">Chill House Summer 2024</span>
-                        <span className="truncate text-wrap text-sm font-normal text-neutral-400">
-                            Summer chill tracks to vibe or dance to in 2024. Updated weekly.
-                        </span>
-                        {/* // TODO: Misc. playlist info */}
-                        <div className="">
-                            <span>creation user - </span>
-                            <span>song count - </span>
-                            <span>approx. listening length</span>
-                        </div>
-                    </div>
-                </div>
-
-                {/* TODO: Music controls */}
-                <div></div>
-
-                {/* // Table */}
-                <div className="bg-black/10 px-4">
-                    {/* Table header */}
-                    {/* TODO: apply the below classes to table header when table header is no longer within viewport */}
-                    {/* className="absolute top-16 w-[calc(100%-32px)] */}
-                    <div
-                        className="
-                            mb-2
-                            hidden
-                            h-[32px]
-                            grid-cols-playlist-table
-                            items-center
-                            gap-x-4
-                            border-b
-                            border-white/50
-                            px-4
-                            text-white/50
-                            md:grid
-                        "
-                    >
-                        <span>#</span>
-                        <span>Title</span>
-                        <span>Album</span>
-                        <span>Length</span>
-                    </div>
-                    {/* // Table body */}
-                    <span className="px-4 md:grid md:grid-cols-playlist-table md:gap-x-4">
-                        {/* // TODO: table row design - for mobile display SONG TITLE COLUMN ONLY */}
-                        Table filled with PlaylistTableRow items
-                    </span>
-                </div>
-            </main>
+            <PlaylistWrapper colour={colour}>
+                {/* TODO: Playlist page content */}
+                <ContentSectionLoading />
+            </PlaylistWrapper>
         </>
     )
 }
---
+++ src/components/content/PlaylistWrapper.tsx
@@ -0,0 +1,145 @@
+import React, { useRef } from 'react'
+
+// import { PlaylistSimplified } from '../../types/Playlist'
+
+import TemplateWrapper from './TemplateWrapper'
+import HeaderSpacer from '../HeaderSpacer'
+import BackgroundGradient from '../BackgroundGradient'
+
+interface PlaylistWrapperProps {
+    // playlist: PlaylistSimplified
+    colour?: string
+    children: React.ReactNode
+}
+
+const PlaylistWrapper: React.FC<PlaylistWrapperProps> = ({ colour, children }) => {
+    const contentRef = useRef(null)
+
+    return (
+        <TemplateWrapper
+            contentRef={contentRef}
+            colour={colour}
+        >
+            {/* Heading container */}
+            <div
+                className="h-fit w-[full] pb-4 md:h-[300px]"
+                style={{
+                    backgroundColor: `rgb(${colour})`,
+                    backgroundImage: 'linear-gradient(transparent 0, rgba(0, 0, 0, .5) 100%)',
+                }}
+            >
+                <HeaderSpacer className="md:hidden" />
+                {/* Heading content */}
+                <section
+                    className="
+                        mx-auto
+                        flex
+                        h-full
+                        max-w-[1400px]
+                        flex-col
+                        px-4
+                        md:flex-row
+                    "
+                >
+                    {/* Image container */}
+                    <div
+                        className="
+                            mb-4
+                            max-h-[288px]
+                            min-h-[128px]
+                            w-[40vw]
+                            min-w-[128px]
+                            max-w-[288px]
+                            self-center
+                            md:mb-0
+                            md:mr-4
+                            md:max-h-[128px]
+                            md:max-w-[128px]
+                            md:self-end
+                        "
+                    >
+                        {/* Image */}
+                        <img
+                            className="
+                                aspect-square
+                                rounded-full
+                                object-cover
+                            "
+                            // src={
+                            //     playlist.images && playlist.images[0]
+                            //         ? playlist.images[0].url
+                            //         : '../src/assets/images/liked.png'
+                            // }
+                            src={'../src/assets/images/liked.png'}
+                        />
+                    </div>
+                    {/* Details container */}
+                    <div
+                        className="
+                            flex
+                            flex-col
+                            gap-y-2
+                            overflow-hidden
+                            md:self-end
+                        "
+                    >
+                        {/* Details */}
+                        <p className="hidden md:block">Playlist</p>
+                        <p
+                            className="
+                                text-3xl
+                                font-bold
+                                md:text-5xl
+                                md:font-extrabold
+                            "
+                        >
+                            Playlist Name
+                        </p>
+                        <p
+                            className="
+                                text-sm
+                                font-normal
+                                text-neutral-400
+                            "
+                        >
+                            Playlist description
+                        </p>
+                        <div
+                            className="
+                                flex
+                                flex-col
+                                gap-x-2
+                                gap-y-2
+                                text-sm
+                                font-normal
+                                md:flex-row
+                            "
+                        >
+                            <p>Creation user </p>
+                            <p>No. of likes</p>
+                            <p className="hidden md:block">song count </p>
+                            <p className="hidden md:block">approx. listening length</p>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            {/* Content container */}
+            <div className="relative z-[1] h-fit w-full">
+                <BackgroundGradient
+                    colour={colour}
+                    className="z-[-1]"
+                    size="large"
+                />
+                {/* Content */}
+                <section
+                    ref={contentRef}
+                    className="mx-auto h-fit w-full max-w-[1400px] p-4"
+                >
+                    {children}
+                </section>
+            </div>
+        </TemplateWrapper>
+    )
+}
+
+export default PlaylistWrapper
