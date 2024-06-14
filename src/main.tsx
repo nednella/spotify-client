4c6430e223f38f22c7927f750b857001fbe117b0
Ben Allenden <benallenden@protonmail.com>
2024-06-14 02:52:55 +0100

    Feat: useLibrary hook & context provider

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import AuthProvider from './providers/AuthProvider'
 import ModalProvider from './providers/ModalProvider'
 import ToastProvider from './providers/ToastProvider'
+import LibraryProvider from './providers/LibraryProvider'
 
 import './main.css'
 import App from './app/App'
@@ -18,7 +19,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
             <AuthProvider>
                 <ModalProvider />
                 <ToastProvider />
-                <App />
+                <LibraryProvider>
+                    <App />
+                </LibraryProvider>
             </AuthProvider>
         </BrowserRouter>
     </QueryClientProvider>
