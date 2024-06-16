156d1aad2eb3ec2c74da7e816f6d50061747abf8
Ben Allenden <benallenden@protonmail.com>
2024-06-16 04:31:13 +0100

    Feat: tabbed navigation menu

--- src/app/pages/Artist.tsx
+++ src/app/pages/Artist.tsx
@@ -2,37 +2,51 @@ import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useQuery } from '@tanstack/react-query'
 
+import useScrollOpacity from '../../hooks/useScrollOpacity'
 import { useAuth } from '../../hooks/useAuth'
+import { useLibrary } from '../../hooks/useLibrary'
+
 import getArtist from '../../api/artist/getArtist'
 
 import Loading from './Loading'
 import NotFound from './NotFound'
-import ArtistWrapper from '../../components/content/ArtistWrapper'
+import ArtistWrapper from '../../components/wrappers/ArtistWrapper'
+import BackgroundColour from '../../components/BackgroundColour'
+import { TabMenu, TabItems, TabTrigger, TabContent } from '../../components/TabMenu'
+import ActionBar from '../../components/ActionBar'
 import ContentSectionLoading from '../../components/homepage/ContentSectionLoading'
 
 const Artist = () => {
-    const { user } = useAuth()
-    const { id: artist_id } = useParams()
+    const { id: artistId } = useParams()
+    const { opacity } = useScrollOpacity()
     const [colour, setColour] = useState<string | undefined>(undefined) // accepts 'r/g/b' format
 
-    const { data, isLoading, isError } = useQuery({
-        queryKey: ['artist', artist_id],
-        queryFn: async () => getArtist(artist_id),
-        enabled: user !== null,
+    const { user } = useAuth()
+    const { data: libraryData, isLoading: libraryLoading, isError: libraryError } = useLibrary()
+    const {
+        data: artistData,
+        isLoading: artistLoading,
+        isError: artistError,
+    } = useQuery({
+        queryKey: ['artist', artistId],
+        queryFn: async () => getArtist(artistId),
+        enabled: user !== null && artistId !== null,
     })
 
-    if (data) {
-        console.log(data)
-    }
-
     useEffect(() => {
-        setColour('16, 88, 114')
+        setColour('56, 144, 176')
+        // setColour('16, 88, 114')
         // setColour('6, 95, 70')
         // setColour('150, 23, 23')
         // setColour('240, 144, 184')
         // setColour('250,230,250')
         // setColour('29, 185, 84')
-    }, [data])
+    }, [artistData])
+
+    const isLoading = libraryLoading || artistLoading
+    const isError = libraryError || artistError
+
+    if (!artistId) return <NotFound />
 
     return isLoading ? (
         <Loading />
@@ -40,13 +54,67 @@ const Artist = () => {
         <NotFound />
     ) : (
         user &&
-        data && (
+        libraryData &&
+        artistData && (
             <ArtistWrapper
-                artist={data.artist}
+                artist={artistData.artist}
                 colour={colour}
             >
-                {/* TODO: Artist page content */}
-                <ContentSectionLoading />
+                <TabMenu>
+                    <TabItems className="sticky top-[64px]">
+                        <BackgroundColour opacity={opacity} />
+                        <div className="mx-auto max-w-[1400px]">
+                            <TabTrigger
+                                value="tab-1"
+                                title="Home"
+                            />
+                            <TabTrigger
+                                value="tab-2"
+                                title="Albums"
+                            />
+                            <TabTrigger
+                                value="tab-3"
+                                title="Singles & EPs"
+                            />
+                            <TabTrigger
+                                value="tab-4"
+                                title="Compilations"
+                            />
+                        </div>
+                    </TabItems>
+                    <TabContent
+                        className="mx-auto max-w-[1400px]"
+                        value="tab-1"
+                    >
+                        <ActionBar
+                            libraryData={libraryData.artists}
+                            contentType="artist"
+                            contentId={artistData.id}
+                        />
+                        <ContentSectionLoading />
+                        <ContentSectionLoading />
+                        <ContentSectionLoading />
+                        <ContentSectionLoading />
+                    </TabContent>
+                    <TabContent
+                        className="mx-auto max-w-[1400px]"
+                        value="tab-2"
+                    >
+                        <p>Content 2</p>
+                    </TabContent>
+                    <TabContent
+                        className="mx-auto max-w-[1400px]"
+                        value="tab-3"
+                    >
+                        <p>Content 3</p>
+                    </TabContent>
+                    <TabContent
+                        className="mx-auto max-w-[1400px]"
+                        value="tab-4"
+                    >
+                        <p>Content 4</p>
+                    </TabContent>
+                </TabMenu>
             </ArtistWrapper>
         )
     )
