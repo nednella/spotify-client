591f59e42ad8702d335420aeff11fe78470f48ca
Ben Allenden <benallenden@protonmail.com>
2024-06-17 20:57:34 +0100

    Feat: TrackList component

---
+++ src/utils/convertTrackDuration.ts
@@ -0,0 +1,14 @@
+/**
+ * Converts track duration from ms to minutes:seconds.
+ * @param {string} ms - Track duration in milliseconds.
+ * @returns Track duration in minutes:seconds format with padded seconds if applicable.
+ */
+export function convertTrackDuration(ms: string) {
+    const millis = Number(ms)
+    const minutes = Math.floor(millis / 1000 / 60).toString()
+    let seconds = Math.floor((millis / 1000) % 60).toString()
+    if (seconds.length === 1) {
+        seconds = `0${seconds}`
+    }
+    return `${minutes}:${seconds}`
+}
