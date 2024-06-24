7ee3cd5714d7e2b54b2f9a7e7a08c6ab1eb1086c
Ben Allenden <benallenden@protonmail.com>
2024-06-24 19:11:55 +0100

    Fix: className indentation on specific wrapper components

--- src/components/wrappers/ArtistWrapper.tsx
+++ src/components/wrappers/ArtistWrapper.tsx
@@ -39,39 +39,39 @@ const ArtistWrapper: React.FC<ArtistWrapperProps> = ({ artist, colour, children 
                     {/* Heading content */}
                     <section
                         className="
-                        mx-auto
-                        flex
-                        h-full
-                        max-w-[1400px]
-                        flex-col
-                        px-4
-                        md:flex-row
-                    "
+                            mx-auto
+                            flex
+                            h-full
+                            max-w-[1400px]
+                            flex-col
+                            px-4
+                            md:flex-row
+                        "
                     >
                         {/* Image container */}
                         <div
                             className="
-                            mb-4
-                            max-h-[288px]
-                            min-h-[128px]
-                            w-[40vw]
-                            min-w-[128px]
-                            max-w-[288px]
-                            self-center
-                            md:mb-0
-                            md:mr-4
-                            md:max-h-[128px]
-                            md:max-w-[128px]
-                            md:self-end
-                        "
+                                mb-4
+                                max-h-[288px]
+                                min-h-[128px]
+                                w-[40vw]
+                                min-w-[128px]
+                                max-w-[288px]
+                                self-center
+                                md:mb-0
+                                md:mr-4
+                                md:max-h-[128px]
+                                md:max-w-[128px]
+                                md:self-end
+                            "
                         >
                             {/* Image */}
                             <img
                                 className="
-                                aspect-square
-                                rounded-full
-                                object-cover
-                            "
+                                    aspect-square
+                                    rounded-full
+                                    object-cover
+                                "
                                 src={
                                     artist.images && artist.images[0]
                                         ? artist.images[0].url
@@ -82,22 +82,22 @@ const ArtistWrapper: React.FC<ArtistWrapperProps> = ({ artist, colour, children 
                         {/* Details container */}
                         <div
                             className="
-                            flex
-                            flex-col
-                            gap-y-2
-                            overflow-hidden
-                            md:self-end
-                        "
+                                flex
+                                flex-col
+                                gap-y-2
+                                overflow-hidden
+                                md:self-end
+                            "
                         >
                             {/* Details */}
                             <p className="hidden md:block">Artist</p>
                             <p
                                 className="
-                                text-3xl
-                                font-bold
-                                md:text-5xl
-                                md:font-extrabold
-                            "
+                                    text-3xl
+                                    font-bold
+                                    md:text-5xl
+                                    md:font-extrabold
+                                "
                             >
                                 {artist.name}
                             </p>
--- src/components/wrappers/ProfileWrapper.tsx
+++ src/components/wrappers/ProfileWrapper.tsx
@@ -38,39 +38,39 @@ const ProfileWrapper: React.FC<ProfileWrapperProps> = ({ user, colour, children 
                     {/* Heading content */}
                     <section
                         className="
-                        mx-auto
-                        flex
-                        h-full
-                        max-w-[1400px]
-                        flex-col
-                        px-4
-                        md:flex-row
-                    "
+                            mx-auto
+                            flex
+                            h-full
+                            max-w-[1400px]
+                            flex-col
+                            px-4
+                            md:flex-row
+                        "
                     >
                         {/* Image container */}
                         <div
                             className="
-                            mb-4
-                            max-h-[288px]
-                            min-h-[128px]
-                            w-[40vw]
-                            min-w-[128px]
-                            max-w-[288px]
-                            self-center
-                            md:mb-0
-                            md:mr-4
-                            md:max-h-[128px]
-                            md:max-w-[128px]
-                            md:self-end
-                        "
+                                mb-4
+                                max-h-[288px]
+                                min-h-[128px]
+                                w-[40vw]
+                                min-w-[128px]
+                                max-w-[288px]
+                                self-center
+                                md:mb-0
+                                md:mr-4
+                                md:max-h-[128px]
+                                md:max-w-[128px]
+                                md:self-end
+                            "
                         >
                             {/* Image */}
                             <img
                                 className="
-                                aspect-square
-                                rounded-full
-                                object-cover
-                            "
+                                    aspect-square
+                                    rounded-full
+                                    object-cover
+                                "
                                 src={
                                     user.images && user.images[1]
                                         ? user.images[1].url
@@ -81,22 +81,22 @@ const ProfileWrapper: React.FC<ProfileWrapperProps> = ({ user, colour, children 
                         {/* Details container */}
                         <div
                             className="
-                            flex
-                            flex-col
-                            gap-y-2
-                            overflow-hidden
-                            md:self-end
-                        "
+                                flex
+                                flex-col
+                                gap-y-2
+                                overflow-hidden
+                                md:self-end
+                            "
                         >
                             {/* Details */}
                             <p className="hidden md:block">Profile</p>
                             <p
                                 className="
-                                text-3xl
-                                font-bold
-                                md:text-5xl
-                                md:font-extrabold
-                            "
+                                    text-3xl
+                                    font-bold
+                                    md:text-5xl
+                                    md:font-extrabold
+                                "
                             >
                                 {user.display_name}
                             </p>
