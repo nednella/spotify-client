5cb260a0eeedb13a0bfeaffa50d18c8f52d654f6
nednella <119449811+nednella@users.noreply.github.com>
2024-06-28 19:38:36 +0100

    Refactor: remove SavedTrack[] type from Library type, replace with Track[].

--- src/types/Library.ts
+++ src/types/Library.ts
@@ -4,13 +4,8 @@ import { PlaylistSimplified } from './Playlist'
 import { Track } from './Track'
 
 export interface Library {
-    tracks: SavedTrack[]
+    tracks: Track[]
     playlists: PlaylistSimplified[]
     albums: Album[]
     artists: Artist[]
-}
-
-interface SavedTrack {
-    added_at: string
-    track: Track
 }
