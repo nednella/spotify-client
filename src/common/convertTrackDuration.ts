b5a728203b665dab1a6e2b437163b4eb2f7be9db
nednella <119449811+nednella@users.noreply.github.com>
2024-06-28 19:37:47 +0100

    Refactor: convertTrackDuration to accept both number and string type argument

--- src/common/convertTrackDuration.ts
+++ src/common/convertTrackDuration.ts
@@ -3,7 +3,7 @@
  * @param {string} ms - Track duration in milliseconds.
  * @returns Track duration in minutes:seconds format with padded seconds if applicable.
  */
-export function convertTrackDuration(ms: string) {
+export function convertTrackDuration(ms: number | string) {
     const millis = Number(ms)
     const minutes = Math.floor(millis / 1000 / 60).toString()
     let seconds = Math.floor((millis / 1000) % 60).toString()
