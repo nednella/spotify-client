4431047dedf040023d3ed5bdf3db54383400bb7e
Ben Allenden <benallenden@protonmail.com>
2024-07-15 20:08:34 +0100

    Feat: Implemented delete a playlist modal for user-created playlists

--- src/providers/ModalProvider.tsx
+++ src/providers/ModalProvider.tsx
@@ -1,9 +1,11 @@
 import LoginModal from '../components/modals/LoginModal'
+import DeletePlaylistModal from '../components/modals/DeletePlaylistModal'
 
 const ModalProvider = () => {
     return (
         <>
             <LoginModal />
+            <DeletePlaylistModal />
         </>
     )
 }
