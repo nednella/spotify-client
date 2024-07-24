ed7321f0fabc877eabbe8392f83149245606a1b0
Ben Allenden <benallenden@protonmail.com>
2024-07-24 21:57:09 +0100

    Refactor: remove user validation from useGetBrowseCategories

--- src/hooks/useGetBrowseCategories.ts
+++ src/hooks/useGetBrowseCategories.ts
@@ -2,13 +2,10 @@ import { useQuery } from '@tanstack/react-query'
 
 import getBrowseCategories from '../api/search/getBrowseCategories'
 
-import { User } from '../types/User'
-
-const useGetBrowseCategories = (user: User | null) => {
+const useGetBrowseCategories = () => {
     return useQuery({
         queryKey: ['browse'],
         queryFn: async () => getBrowseCategories(),
-        enabled: user !== null,
         staleTime: 600000, // 1000 * 60 * 10 minutes
     })
 }
