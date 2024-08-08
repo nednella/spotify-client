bd1d3d2a065818027962be638aeab3d7e2dfe4ef
nednella <119449811+nednella@users.noreply.github.com>
2024-08-08 04:35:14 +0100

    Feat: Spotify SDK download

---
+++ src/spotifySDK/spotifySDK.ts
@@ -0,0 +1,35 @@
+export default window.onSpotifyWebPlaybackSDKReady = (token: string): Spotify.Player => {
+    const player = new Spotify.Player({
+        name: 'Spotify Clone',
+        getOAuthToken: (cb) => {
+            cb(token)
+        },
+        volume: 0.25,
+    })
+
+    player.addListener('ready', ({ device_id }) => {
+        console.log('Connected with Device ID', device_id)
+    })
+
+    player.addListener('player_state_changed', (state) => {
+        console.log('Player state changed', state)
+    })
+
+    player.on('initialization_error', ({ message }) => {
+        console.log('Failed to initialize', message)
+    })
+
+    player.on('authentication_error', ({ message }) => {
+        console.error('Failed to authenticate', message)
+    })
+
+    player.on('account_error', ({ message }) => {
+        console.error('Failed to validate Spotify account', message)
+    })
+
+    player.on('playback_error', ({ message }) => {
+        console.error('Failed to perform playback', message)
+    })
+
+    return player
+}
