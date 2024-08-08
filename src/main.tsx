b32dde40488a35325c1ee0fcd6eff6de9bdfefe6
Ben Allenden <benallenden@protonmail.com>
2024-08-08 04:35:14 +0100

    Feat: Spotify SDK download

--- src/main.tsx
+++ src/main.tsx
@@ -13,6 +13,12 @@ import App from './app/App'
 
 const queryClient = new QueryClient()
 
+// Spotify Web Playback SDK download
+const script = document.createElement('script')
+script.src = 'https://sdk.scdn.co/spotify-player.js'
+script.async = true
+document.body.appendChild(script)
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
         <BrowserRouter>
