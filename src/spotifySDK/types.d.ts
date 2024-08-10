fd557d6580f53e81add0ba3c11272ffb377f4e9f
nednella <119449811+nednella@users.noreply.github.com>
2024-08-10 17:46:33 +0100

    Add: missing Spotify.PlaybackState.disallows types

--- src/spotifySDK/types.d.ts
+++ src/spotifySDK/types.d.ts
@@ -42,6 +42,9 @@ declare namespace Spotify {
             seeking: boolean
             skipping_next: boolean
             skipping_prev: boolean
+            toggling_repeat_track: boolean
+            toggling_repeat_context: boolean
+            toggling_shuffle: boolean
         }
         duration: number
         loading: boolean
