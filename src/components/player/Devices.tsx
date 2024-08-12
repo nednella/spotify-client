7e6941e5a5f095f0f33162e6ab741431cd7e3378
Ben Allenden <benallenden@protonmail.com>
2024-08-12 21:51:39 +0100

    Feat: Connect to a device functionality

--- src/components/player/Devices.tsx
+++ src/components/player/Devices.tsx
@@ -1,5 +1,33 @@
+import { FiSpeaker } from 'react-icons/fi'
+
+import usePlayer from '../../hooks/usePlayer'
+
+import DeviceMenu from '../menus/DeviceMenu'
+
+import Tooltip from '../Tooltip'
+
 const Devices = () => {
-    return <div className="w-[30%]">Devices</div>
+    const player = usePlayer()
+    return (
+        <div className="flex w-[30%] items-center justify-end pr-2">
+            <DeviceMenu>
+                <button
+                    data-active={!player.isActiveDeviceExternal()}
+                    className="
+                        text-neutral-400
+                        hover:text-white
+                        data-[active=true]:text-green-500
+                    "
+                >
+                    <Tooltip message={'Connect to a device'}>
+                        <span>
+                            <FiSpeaker size={20} />
+                        </span>
+                    </Tooltip>
+                </button>
+            </DeviceMenu>
+        </div>
+    )
 }
 
 export default Devices
