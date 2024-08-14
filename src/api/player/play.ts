effb48568fecc8f87bdf144253cf577ea507399a
nednella <119449811+nednella@users.noreply.github.com>
2024-08-14 17:59:53 +0100

    Remove: console log

--- src/api/player/play.ts
+++ src/api/player/play.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 export default async function play(deviceId: string | null, contextUri?: string, offset?: number) {
-    console.log(deviceId, contextUri, offset)
     return await axios.put(
         `http://localhost:5000/player/play`,
         {},
