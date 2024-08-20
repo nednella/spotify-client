854387fbfa7e70e9dd3697d13ecb1da930e98330
nednella <119449811+nednella@users.noreply.github.com>
2024-08-20 21:13:38 +0100

    Add boolean check to seeker progress & fix float value bug on slider commit

--- src/components/player/Seeker.tsx
+++ src/components/player/Seeker.tsx
@@ -9,16 +9,17 @@ import Slider from '../Slider'
 const Seeker = () => {
     const player = usePlayer()
     const playerState = player.playerState
+    const isThisDeviceActive = player.isThisDeviceActive()
 
     useEffect(() => {
         const progress = setInterval(() => {
-            if (!playerState?.paused) {
+            if (isThisDeviceActive && !playerState?.paused) {
                 player.updatePlayerPos(playerState?.position + 1000)
             }
         }, 1000)
 
         return () => clearInterval(progress)
-    }, [player, playerState])
+    }, [player, playerState, isThisDeviceActive])
 
     return (
         <div className="flex items-center justify-center gap-x-2 text-sm text-neutral-300">
@@ -28,8 +29,8 @@ const Seeker = () => {
                 value={playerState?.position / playerState?.duration}
                 thumb
                 onChange={(v) => player.updatePlayerPos(v * playerState?.duration)}
-                onCommit={(v) => player.seek(v * playerState?.duration)}
-                disabled={playerState?.disallows.seeking}
+                onCommit={(v) => player.seek(Math.round(v * playerState?.duration))}
+                disabled={!isThisDeviceActive && playerState?.disallows.seeking}
                 className="w-full max-w-[600px]"
             />
             <span className="w-10 text-nowrap">{convertTrackDuration(playerState?.duration) || '-:--'}</span>
