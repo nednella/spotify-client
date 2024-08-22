4a2342f3040174c50d64630a366764a040419e26
Ben Allenden <benallenden@protonmail.com>
2024-08-22 17:02:37 +0100

    Fix: missing JDoc comment

--- src/common/generatePlaylistName.ts
+++ src/common/generatePlaylistName.ts
@@ -1,9 +1,10 @@
-/**
- *
- */
-
 import { SimplifiedPlaylist } from '../types/Playlist'
-
+/**
+ * Generate playlist name when new playlist is created.
+ * @param {string } user_id - The Spotify ID of the authenticated user.
+ * @param {SimplifiedPlaylist[]} playlists - An array of the user's playlists.
+ * @returns a string for the newly generated playlist.
+ */
 export function generatePlaylistName(user_id: string, playlists: SimplifiedPlaylist[]) {
     const userPlaylistsCount = playlists.filter((playlist: SimplifiedPlaylist) => playlist.owner.id === user_id).length
 
